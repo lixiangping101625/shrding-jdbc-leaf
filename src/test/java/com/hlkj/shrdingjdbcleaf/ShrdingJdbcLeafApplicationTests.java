8d616f43a92d23e21dbd690264de12c0ef528ce1
lixiangping <admin>
2022-03-31 15:25:54 +0800

    sharding-jdbc分布式事务测试

--- src/test/java/com/hlkj/shrdingjdbcleaf/ShrdingJdbcLeafApplicationTests.java
+++ src/test/java/com/hlkj/shrdingjdbcleaf/ShrdingJdbcLeafApplicationTests.java
@@ -1,13 +1,40 @@
 package com.hlkj.shrdingjdbcleaf;
 
+import com.hlkj.shrdingjdbcleaf.pojo.TOrder;
+import com.hlkj.shrdingjdbcleaf.repository.TOrder0Repository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class ShrdingJdbcLeafApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+    @Autowired
+    private TOrder0Repository order0Repository;
+
+    @Test
+    @Transactional
+    void testShardingTransaction(){
+        TOrder order = new TOrder();
+//        order.setId(1L);
+        order.setTitle("测试订单");
+        order.setUserId(11L);
+        TOrder orderSave = order0Repository.save(order);
+
+        TOrder order2 = new TOrder();
+//        order2.setId(2L);
+        order2.setTitle("测试订单");
+        order2.setUserId(12L);
+
+        TOrder orderSave2 = order0Repository.save(order2);
+
+        throw new RuntimeException("测试shardingjdbc 事务");
     }
 
 }
